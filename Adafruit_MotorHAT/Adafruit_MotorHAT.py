a0bfe5eb6073363a7bd1e76facc532121549986c
lciscon <lciscon@yahoo.com>
2023-03-30 16:23:25 -0500

    Added bus number param to MotorHAT initialization routines to support multiple HATs

--- Adafruit_MotorHAT/Adafruit_MotorHAT.py
+++ Adafruit_MotorHAT/Adafruit_MotorHAT.py
@@ -224,12 +224,12 @@ class Adafruit_MotorHAT:
 	INTERLEAVE = 3
 	MICROSTEP = 4
 
-	def __init__(self, addr = 0x60, freq = 1600):
+	def __init__(self, addr = 0x60, busnum = 1, freq = 1600):
 		self._i2caddr = addr            # default addr on HAT
 		self._frequency = freq		# default @1600Hz PWM freq
 		self.motors = [ Adafruit_DCMotor(self, m) for m in range(4) ]
 		self.steppers = [ Adafruit_StepperMotor(self, 1), Adafruit_StepperMotor(self, 2) ]
-		self._pwm =  PWM(addr, debug=False)
+		self._pwm =  PWM(addr, busnum, debug=False)
 		self._pwm.setPWMFreq(self._frequency)
 
 	def setPin(self, pin, value):
